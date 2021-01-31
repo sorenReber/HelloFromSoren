253d8897f85c469e8a95727e62f4c828664c3102
drbaranyaid <drbaranyai.d@gmail.com>
2021-01-31 08:22:10 +0100

    edited by Dora

--- HelloFromSoren/src/Hello/Main.java
+++ HelloFromSoren/src/Hello/Main.java
@@ -3,7 +3,8 @@ package Hello;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Soren.");
+        System.out.println("Hello, my name is Soren.");
+        System.out.println("Hello Soren, my name is Dora.");
     }
 }
 
